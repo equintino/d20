2aafc7a37df70c2b85ce571be3fe3259d1ee8362
equintino <emquintino@ig.com.br>
2017-05-04 10:47:03 -0300

    main

--- main.php
+++ main.php
@@ -6,8 +6,8 @@
 <?php 
 include 'validacao/valida_cookies.php';
 $acesso = new valida_cookies();
-$acesso->setLogin($_POST['login']);
-$acesso->setSenha($_POST['senha']);
+@$acesso->setLogin($_POST['login']);
+@$acesso->setSenha($_POST['senha']);
 if (!$acesso->getLogin()){
     $acesso->popup('Você deve entrar com o usuário.',null);
 }else{
