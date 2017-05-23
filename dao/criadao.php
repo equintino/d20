c64f03ea50c989b1f38d42c94743b70df8c72942
equintino <emquintino@ig.com.br>
2017-05-23 14:53:19 -0300

    avatar

--- dao/criadao.php
+++ dao/criadao.php
@@ -111,7 +111,11 @@
         return $model;
    }'."\r\n"; 
      $texto .= '  public function encontrePorMissao(ModelSearchCriteria $search=null){
+           if($search->getpersonagem()){
            $row = $this->query("SELECT * FROM `".$search->gettabela()."` WHERE `excluido` = \'0\' and `MISSAO` = \'".$search->getMISSAO()."\' and personagem = \'".$search->getpersonagem()."\'")->fetch();
+         }else{
+           $row = $this->query("SELECT * FROM `".$search->gettabela()."` WHERE `excluido` = \'0\' and `MISSAO` = \'".$search->getMISSAO()."\'")->fetch();
+         }
         if (!$row) {
             return null;
         }
